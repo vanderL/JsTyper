7a7630fe8a609f12a733184ea03b6f67debb1b15
vander <vnd.vander@gmail.com>
2017-01-05 03:32:35 -0200

    Contador de caractere e palavras

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 var frase = jQuery(".frase").text();
 var contador = frase.split(" ");
-
 var localFrase = $("#tamanho");
 localFrase.text(contador.length);
-console.log(localFrase);
+
+var campo = $(".campo-digitado");
+	campo.on("input", function(){
+		
+		var conteudo = campo.val();
+
+		var qtdPalavras = conteudo.split(/\S+/).length -1;
+		$("#contador-palavra").text(qtdPalavras);
+
+		var qtdCaracteres = conteudo.length;
+		$("#contador-caractere").text(qtdCaracteres);
+
+	})
