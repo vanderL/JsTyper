b45cc353abff694eeb1b5f7544c8ed91760c0762
vander <vnd.vander@gmail.com>
2017-01-05 22:32:13 -0200

    Logica do cronometro

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//logica do contado de caracteres e palavras
+
 var frase = jQuery(".frase").text();
 var contador = frase.split(" ");
 var localFrase = $("#tamanho");
@@ -14,4 +16,18 @@ var campo = $(".campo-digitado");
 		var qtdCaracteres = conteudo.length;
 		$("#contador-caractere").text(qtdCaracteres);
 
-	})
+	});
+
+//  logica do cronometro
+var tempoRestante = $("#tempo").text();
+campo.one("focus", function(){
+	var cronometroID = setInterval(function(){
+		tempoRestante--;
+		console.log(tempoRestante);
+		$("#tempo").text(tempoRestante);
+		if (tempoRestante < 1) {
+			campo.attr("disabled", true);
+			clearInterval(cronometroID);
+		}
+	}, 1000);
+});
